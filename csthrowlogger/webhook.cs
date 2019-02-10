c0fae1e0e948933698c21143a02cba6c3e6d5708
Assasindie <xmanbutler@gmail.com>
2019-02-10 19:12:15 +1000

    Added try catch to sending via webhook
    pls use the right webhook friends :smile:

--- csthrowlogger/webhook.cs
+++ csthrowlogger/webhook.cs
@@ -1,15 +1,26 @@
 ﻿using Discord.Webhook;
+using System;
 using System.IO;
 
 namespace csthrowlogger
 {
     class webhook
     {
-        static DiscordWebhookClient discord = new DiscordWebhookClient(544040123864711181, "25UL8swXTHQuZOLlP3lXlx9NIWqkwVU_QM9EDqo8EvgJUkm0DrvJd4SGbXApygiE3kMo");
 
         public static void SendMessage(string message)
         {
-            discord.SendMessageAsync(message);
+            try
+            {
+                DiscordWebhookClient discord = new DiscordWebhookClient(544040123864711181, "25UL8swXTHQuZOLlP3lXlx9NIWqkwVU_QM9EDqo8EvgJUkm0DrvJd4SGbXApygiE3kMo");
+                discord.SendMessageAsync(message);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("OwO Something went wrong sending the information to discord! Check to see if your webhook credentials are correct! Press any key to exit");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
         }
     }
 }
